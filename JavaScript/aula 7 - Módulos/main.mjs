3010d83615960ced7604f7b2650a041d79c852b2
Ni-cole17 <nicole.charron17@gmail.com>
2022-04-01 13:11:46 -0300

    Módulos

---
+++ JavaScript/aula 7 - Módulos/main.mjs
@@ -0,0 +1,5 @@
+import {    mostraCidade,    mostraIdade,    mostraHobby} from "./funcoes.mjs"
+
+console.log(mostraIdade('Nicole',22))
+console.log(mostraCidade('Nicole','Recife'))
+console.log(mostraHobby('Nicole','Jogar'))
